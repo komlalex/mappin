4cf3dadbe5cc46f0b8bd234ff744bafb2433677e
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-06-11 07:50:29 +0000

    User schema completed

---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 6
+    }
+}, {timestamps: true})
