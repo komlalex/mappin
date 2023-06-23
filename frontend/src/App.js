f7872513809a07a381600287e63620fe1f7e69b3
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-06-23 01:53:19 +0000

    Basic mapbox set-up established

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-
-
+import {useState} from "react";
+import Map, {Marker} from "react-map-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
 function App() {
+  const [viewstate, setViewstate] = useState({
+    latitude: 48,
+      longitude: 17,     
+      zoom: 4
+  }) 
+
   return (
-    <div>App</div>
+    <Map 
+    {...viewstate}
+    onMove={evt => setViewstate(evt.viewState)}
+    style={{width: 800, height: 600}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+    mapboxAccessToken= {process.env.REACT_APP_MAPBOX}  
+    >
+      <Marker latitude={48.858093} longitude={2.294694} color="red" />
+    </Map>
   )
 }
 
